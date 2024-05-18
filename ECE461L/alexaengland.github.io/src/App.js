876dc633c7cb040defff1c5d2c2fec53b58ed744
= <alexaengland29@utexas.edu>
2024-05-18 13:48:10 -0500

    Committed website changes

---
+++ ECE461L/alexaengland.github.io/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Education from './components/Education';
+import ExperienceAndSkills from './components/ExperienceAndSkills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="main-content pt-5">
+        <section id="home">
+          <Home />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="education">
+          <Education />
+        </section>
+        <section id="experience-and-skills">
+          <ExperienceAndSkills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+      <footer className="bg-dark text-white p-4 text-center">
+        <p>© 2024 Alexa England</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ ECE461L/alexaengland.github.io/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="About bg-light p-5 rounded shadow m-4 mx-auto text-center">
+      <h2 className="display-4 fw-bold mb-4">About Me</h2>
+      <p className="lead">Hello, I'm Alexa England, an electrical and computer engineer with a passion for data science and machine learning. I am currently pursuing my Bachelor's degree at The University of Texas at Austin, I aspire to leverage my skills in software engineering to drive innovation in technology.</p>
+    </div>
+  );
+}
+
+export default About;
+
+
+
+
