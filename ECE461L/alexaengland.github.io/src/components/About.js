fb26edf8d9974494c35d4ca16c4ac57ee81ed2b1
= <alexaengland29@utexas.edu>
2024-05-18 16:41:32 -0500

    changed design

--- ECE461L/alexaengland.github.io/src/components/About.js
+++ ECE461L/alexaengland.github.io/src/components/About.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './About.css';
 
 function About() {
   return (
-    <div className="About bg-light p-5 rounded shadow m-4 mx-auto text-center">
+    <div className="About bg-blue-100 p-5 rounded shadow mx-auto text-center h-144">
       <h2 className="display-4 fw-bold mb-4">About Me</h2>
       <p className="lead">Hey! I'm Alexa England, an electrical and computer engineer with a passion for data science and machine learning. I am currently pursuing my Bachelor's degree at The University of Texas at Austin, I aspire to leverage my skills in software engineering to drive innovation in technology.</p>
     </div>
