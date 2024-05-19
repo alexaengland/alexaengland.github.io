a672d358a916ec21a68dd9746d1f7fb69883df3b
= <alexaengland29@utexas.edu>
2024-05-19 00:40:39 -0500

    Changed ABout section height

--- ECE461L/alexaengland.github.io/src/components/About.js
+++ ECE461L/alexaengland.github.io/src/components/About.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="About bg-dark p-5 rounded shadow mx-auto text-center h-144">
-      <div className="text-center">
-          <h2 className="display-4 font-bold text-white mb-4">About Me</h2>
-          <p className="lead text-white">Hey! I'm Alexa England, an electrical and computer engineer with a passion for data science and machine learning. I am currently pursuing my Bachelor's degree at The University of Texas at Austin, I aspire to leverage my skills in software engineering to drive innovation in technology.</p>
-          </div>
+    <div className="About bg-dark p-5 rounded shadow mx-auto text-center flex items-center justify-center" style={{ minHeight: '20rem' }}>
+      <div>
+        <h2 className="display-4 font-bold text-white mb-4">About Me</h2>
+        <p className="lead text-white">Hey! I'm Alexa England, an electrical and computer engineer with a passion for data science and machine learning. I am currently pursuing my Bachelor's degree at The University of Texas at Austin, and I aspire to leverage my skills in software engineering to drive innovation in technology.</p>
+      </div>
     </div>
   );
 }
 
 export default About;
+
+
